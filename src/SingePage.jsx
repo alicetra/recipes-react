cbf7dff99d031eb5c3af993322ba97fc8959d569
alicetra <alicetra0109@gmail.com>
2024-05-31 21:21:49 +1000

    layout all the components in the single page

--- src/SingePage.jsx
+++ src/SingePage.jsx
@@ -1,6 +1,16 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
 import { useSelector } from 'react-redux';
+import RecipeTitle from './components/RecipeTitle';
+import RecipeDifficulty from './components/RecipeDifficulty';
+import RecipeRating from './components/RecipeRating';
+import RecipeCuisine from './components/RecipeCuisine';
+import RecipeImage from './components/RecipeImage';
+import RecipeReview from './components/RecipeReview';
+import RecipeServing from './components/RecipeServing';
+import RecipeTime from './components/RecipeTime';
+import RecipeInstruction from './components/RecipleInstruction';
+import RecipeIngredient from './components/RecipeIngredient';
 
 const SinglePage = () => {
   const { recipe_id } = useParams();
@@ -14,9 +24,27 @@ const selectedItem = state.data.recipes.find(recipe => recipe.id === numericReci
   console.log("All Recipes:", state.data.recipes);
   console.log("Selected Item:", selectedItem);
 
-  if (!selectedItem) {
-    return <div>Item not found.</div>;
-  }
-}
+  
+  return (
+    <div>
+      {state.isLoading ? (
+        <h1>Loading...</h1>
+      ) : (
+        <div>
+        <RecipeTitle name={selectedItem.name} id={selectedItem.id} />
+        <RecipeDifficulty difficulty={selectedItem.difficulty} />
+        <RecipeRating rating={selectedItem.rating} />
+        <RecipeCuisine cuisine={selectedItem.cuisine} />
+        <RecipeReview review={selectedItem.reviewCount} />
+        <RecipeServing serving={selectedItem.servings} />
+        <RecipeTime time={selectedItem.prepTimeMinutes} />
+        <RecipeImage image={selectedItem.image} />
+        <RecipeIngredient ingredient={selectedItem.ingredients} />
+        <RecipeInstruction instruction={selectedItem.instructions} />
+        </div>
+      )}
+    </div>
+  );
+};
 
 export default SinglePage;
